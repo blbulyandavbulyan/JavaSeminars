a5fd9d269a9f22933a068dea84b767a26b61f0fb
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-08 14:24:25 +0400

    Method for finding prime numbers from 1 to n added

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar1/homework/Numbers.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar1/homework/Numbers.java
@@ -1,18 +1,20 @@
 package org.blbulyandavbulyan.jseminars.seminar1.homework;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * Данный класс содержит методы для решения задач 1 и 2
  * */
 public class Numbers {
     /**
+     * Вспомогательный метод для проверки на валидность параметров from, to используется в других функциях этого класса
      * @param from нижняя граница
      * @param to верхня граница
      * @throws IllegalArgumentException если from > to
      * */
-    private static void throwIfFromLargeThanTo(int to, int from){
-        if(from > to)throw new IllegalArgumentException("Нижняя граница больше верхней");
+    private static void throwIfFromLargeThanTo(int from, int to){
+        if(from > to)throw new IllegalArgumentException("%d должно быть не больше %d".formatted(from, to));
     }
     /**
      * Считает сумму целых чисел от from до to
@@ -38,6 +40,20 @@ public class Numbers {
         return findSum(1, n);
     }
     /**
+     * Проверяет, является ли n простым
+     * @param n натуральное число для проверки
+     * @return true если n простое, иначе false
+     * */
+    public static boolean isPrime(int n){
+        throwIfFromLargeThanTo(1, Integer.MAX_VALUE);
+        for (int i = 2; i <= n; i++) {
+            if(n % i == 0)return false;
+        }
+        return n != 1;
+    }
+
+
+    /**
      * Ищет все простые числа от from до to
      * @param from нижняя граница(включительно)
      * @param to верхняя граница(включительно)
@@ -45,7 +61,21 @@ public class Numbers {
      * @throws IllegalArgumentException если from > to
      * */
     public static Collection<Integer> findPrimeNumbers(int from, int to){
-        // FIXME: 08.06.2023 Исправить реализацию этой функции
-        return null;
+        throwIfFromLargeThanTo(from, to);
+        Collection<Integer> result = new LinkedList<>();
+        for(int i = from; i <= to; i++){
+            if(isPrime(i))
+                result.add(i);
+        }
+        return result;
+    }
+    /**
+     * Ищет простые числа от 1 до n
+     * @param n верхняя граница(включительно)
+     * @return простые числа в промежутке [1;n]
+     * @throws IllegalArgumentException если не натуральное (n < 1)
+     * */
+    public static Collection<Integer> findPrimeNumbersFromOne(int n){
+        return findPrimeNumbers(1, n);
     }
 }
