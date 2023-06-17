48412c0014cc29b864e2a13ecd13e71b98d74549
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-17 15:50:24 +0400

    Merge sorting and test for it added, but it doesn't work

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar3/homework/utils/Lists.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar3/homework/utils/Lists.java
@@ -44,12 +44,47 @@ public class Lists {
         return iList.stream().collect(Collector.of(MinMaxAverageForStreams::new, MinMaxAverageForStreams::putInteger, MinMaxAverageForStreams::combineToAnother));
     }
 
-    public static void mergeSort(List<Integer> list) {
+    private static <T extends Comparable<T>> List<T> mergeSort(List<T> list, List<T> buffer) {
+        // FIXME: 17.06.2023 Здесь почему-то вылетает ConcurrentModificationException
+        if(list.size() > 1) {
+            int middleIndex = list.size()/2;
+            List<T> first = list.subList(0, middleIndex);
+            List<T> second = list.subList(middleIndex, list.size());
+            var firstSorted = mergeSort(first, buffer);
+            first.clear();
+            first.addAll(firstSorted);
+            var secondSorted= mergeSort(second, buffer);
+            second.clear();
+            second.addAll(secondSorted);
+            merge(first, second, buffer);
+            list.clear();
+            list.addAll(buffer);
+            buffer.clear();
 
+        }
+//        else if()
+        return buffer;
     }
 
-    public static <T extends Comparable<T>> List<T> merge(List<T> l1, List<T> l2) {
-        List<T> result = new ArrayList<>(l1.size() + l2.size());
+    /**
+     * Сортирует список list методом сортировки слиянием
+     *
+     * @param list изменяемый список для сортировки
+     * @param <T>  тип элемента в списке, должен быть Comparable
+     * @return отсортированный список
+     */
+    public static <T extends Comparable<T>> List<T> mergeSort(List<T> list) {
+        return mergeSort(list, new ArrayList<>(list.size()));
+    }
+
+    /**
+     * Приватная часть метода для слияния
+     * @param l1 первый список
+     * @param l2 второй список
+     * @param buffer список, который будет содержать результат слияния
+     * @param <T> тип элементов в списках
+     */
+    private static <T extends Comparable<T>> void merge(List<T> l1, List<T> l2, List<T> buffer) {
         var firstIter = l1.iterator();
         var secondIter = l2.iterator();
         T lastNotAddedValue = null;
@@ -62,42 +97,52 @@ public class Lists {
                 if (needNextSecond) second = secondIter.next();
                 int comparingResult = first.compareTo(second);
                 if (comparingResult < 0) {//first < second
-                    result.add(first);
+                    buffer.add(first);
                     needNextFirst = true;
                     needNextSecond = false;
-                }
-                else if (comparingResult > 0) {//first > second
-                    result.add(second);
+                } else if (comparingResult > 0) {//first > second
+                    buffer.add(second);
                     needNextFirst = false;
                     needNextSecond = true;
-                }
-                else {//first == second
-                    result.add(first);
-                    result.add(second);
+                } else {//first == second
+                    buffer.add(first);
+                    buffer.add(second);
                     needNextFirst = needNextSecond = true;
                 }
                 //это условие нужно, если у нас внезапно закончились элементы из списка, то нам нужно добавить масимальный из двух first или second, поскольку мы добавляем всегда минимальный из first и second
                 //на предыдущих шагах
-                if((!firstIter.hasNext() || !secondIter.hasNext()) && comparingResult != 0){
+                if ((!firstIter.hasNext() || !secondIter.hasNext()) && comparingResult != 0) {
                     lastNotAddedValue = comparingResult > 0 ? first : second;
-                    if (!firstIter.hasNext() && !secondIter.hasNext()) result.add(lastNotAddedValue);
+                    if (!firstIter.hasNext() && !secondIter.hasNext()) buffer.add(lastNotAddedValue);
                 }
             }
             var iter = firstIter.hasNext() ? firstIter : secondIter;
             while (iter.hasNext()) {
                 T current = iter.next();
                 int comparing = lastNotAddedValue != null ? current.compareTo(lastNotAddedValue) : -1;
-                if(comparing < 0) result.add(current);
-                else{
-                    result.add(lastNotAddedValue);
-                    result.add(current);
+                if (comparing < 0) buffer.add(current);
+                else {
+                    buffer.add(lastNotAddedValue);
+                    buffer.add(current);
                     lastNotAddedValue = null;
                 }
-                if(!iter.hasNext() && lastNotAddedValue != null)result.add(lastNotAddedValue);
+                if (!iter.hasNext() && lastNotAddedValue != null) buffer.add(lastNotAddedValue);
             }
 
         }
+    }
 
+    /**
+     * Данный метод предоставляет возможность слить два отсортированных списка в один отсортированный список
+     *
+     * @param l1  первый список для объединения
+     * @param l2  второй список для объединения
+     * @param <T> тип элемента в списках, должен быть Comparable
+     * @return отсортированный список, со всеми элементами из l1 и l2, и размер которого равен l1.size() + l2.size()
+     */
+    public static <T extends Comparable<T>> List<T> merge(List<T> l1, List<T> l2) {
+        List<T> result = new ArrayList<>(l1.size() + l2.size());
+        merge(l1, l2, result);
         return result;
     }
 }
--- src/test/java/org/blbulyandavbulyan/jseminars/seminar3/homework/utils/MergeSortTest.java
+++ src/test/java/org/blbulyandavbulyan/jseminars/seminar3/homework/utils/MergeSortTest.java
@@ -5,13 +5,13 @@ import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
+import static org.blbulyandavbulyan.jseminars.seminar3.homework.utils.Lists.mergeSort;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.blbulyandavbulyan.utils.Lists.generateRandomNumbers;
 
-public class MergeTest {
-    private <T extends Comparable<T>>void check(List<T> first, List<T> second){
+public class MergeSortTest {
+    private <T extends Comparable<T>>void mergeCheck(List<T> first, List<T> second){
         first.sort(T::compareTo);
         second.sort(T::compareTo);
         List<T> expected = new ArrayList<>();
@@ -30,25 +30,33 @@ public class MergeTest {
     public void mergeTest(){
         List<Integer> first = generateRandomNumbers(40, 0, 10);
         List<Integer> second = generateRandomNumbers(30, 0, 20);
-        check(first, second);
+        mergeCheck(first, second);
     }
     @Test
     public void specialMergeTest(){
         List<Integer> first = new ArrayList<>(List.of(1, 2, 8, 20, 22, 23, 36, 40, 42, 43, 46, 56, 67, 71, 72, 78, 81, 102, 105, 115, 115, 117, 123, 137, 143, 145, 152, 172, 183, 198));
         List<Integer> second = new ArrayList<>(List.of(2, 10, 15, 21, 35, 67, 68, 69, 71, 78, 87, 91, 91, 91, 98, 106, 115, 131, 140, 144, 145, 157, 168, 179, 180, 180, 190, 194, 194, 199));
-        check(first, second);
+        mergeCheck(first, second);
     }
     @Test
     public void specialMergeTest2(){
-        check(new ArrayList<>(List.of(17, 20, 30, 30, 54, 54, 65, 65, 65, 70, 76, 77, 88, 89, 105, 120, 120, 124, 126, 133, 149, 153, 155, 163, 164, 178, 187, 189, 191, 191)), new ArrayList<>(List.of(11, 12, 13, 20, 22, 31, 31, 36, 37, 45, 51, 54, 57, 66, 68, 69, 83, 102, 104, 107, 123, 147, 151, 158, 162, 171, 173, 175, 179, 197)));
+        mergeCheck(new ArrayList<>(List.of(17, 20, 30, 30, 54, 54, 65, 65, 65, 70, 76, 77, 88, 89, 105, 120, 120, 124, 126, 133, 149, 153, 155, 163, 164, 178, 187, 189, 191, 191)), new ArrayList<>(List.of(11, 12, 13, 20, 22, 31, 31, 36, 37, 45, 51, 54, 57, 66, 68, 69, 83, 102, 104, 107, 123, 147, 151, 158, 162, 171, 173, 175, 179, 197)));
     }
     @Test
     public void specialMergeTest3(){
-        check(new ArrayList<>(List.of(3, 12, 20, 21, 25, 29, 39, 42, 68, 71, 74, 85, 88, 89, 95, 101, 112, 131, 135, 140, 142, 157, 162, 163, 165, 177, 182, 186, 194, 199)), new ArrayList<>(List.of(2, 4, 8, 13, 17, 18, 38, 47, 54, 55, 57, 59, 64, 76, 82, 82, 85, 90, 91, 99, 104, 110, 111, 144, 159, 163, 164, 175, 194, 197)));
+        mergeCheck(new ArrayList<>(List.of(3, 12, 20, 21, 25, 29, 39, 42, 68, 71, 74, 85, 88, 89, 95, 101, 112, 131, 135, 140, 142, 157, 162, 163, 165, 177, 182, 186, 194, 199)), new ArrayList<>(List.of(2, 4, 8, 13, 17, 18, 38, 47, 54, 55, 57, 59, 64, 76, 82, 82, 85, 90, 91, 99, 104, 110, 111, 144, 159, 163, 164, 175, 194, 197)));
     }
     @Test
     public void firstAndSecondEquals(){
         List<Integer> first = generateRandomNumbers(40, Integer.MIN_VALUE, Integer.MAX_VALUE);
-        check(first, first);
+        mergeCheck(first, first);
+    }
+    @Test
+    public void mergeSortTest(){
+        List<Integer> expected = generateRandomNumbers(100, -200, 200);
+        List<Integer> actual = new ArrayList<>(expected);
+        expected.sort(Integer::compareTo);
+        actual = mergeSort(actual);
+        assertEquals(expected, actual);
     }
 }
