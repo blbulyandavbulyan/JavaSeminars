d21a9b24ac5491349332c224af0858c48773fb7e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-18 14:02:09 +0400

    Simple queue interface added for seminar 4 homework

---
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar4/homework/queue/Queue.java
@@ -0,0 +1,25 @@
+package org.blbulyandavbulyan.jseminars.seminar4.homework.queue;
+
+/**
+ * Данный интерфейс предоставляет мою очередь
+ * @param <T> тип элемента в очереди
+ */
+public interface Queue<T> {
+    /**
+     * Помещает элемент в конец очереди
+     * @param element который нужно поместить
+     */
+    void enqueue(T element);
+
+    /**
+     * Удаляет первый элемент из очереди
+     * @return удалённый элемент
+     */
+    T dequeue();
+
+    /**
+     * Находит первый элемент в очереди и не удаляет его
+     * @return первый элемент
+     */
+    T first();
+}
