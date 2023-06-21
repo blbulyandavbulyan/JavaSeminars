c924d1994d9455fa25ae75d55710564213910324
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-21 18:58:49 +0400

    Unused imports was removed

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/heap/HeapSort.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/heap/HeapSort.java
@@ -1,9 +1,5 @@
 package org.blbulyandavbulyan.jseminars.seminar5.homework.heap;
 
-import java.util.Arrays;
-
-import static org.blbulyandavbulyan.utils.Lists.generateRandomNumbers;
-
 /**
  * Данный класс реализует сортировку с помощью двоичной кучи
  * За основу была взята <a href="https://ru.wikipedia.org/wiki/%D0%94%D0%B2%D0%BE%D0%B8%D1%87%D0%BD%D0%B0%D1%8F_%D0%BA%D1%83%D1%87%D0%B0">статья</a>
