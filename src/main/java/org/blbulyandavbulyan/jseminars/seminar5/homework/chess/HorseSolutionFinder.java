b0561ab52cacffc0dd823642eabcfa873892b130
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-23 14:18:40 +0400

    Some refactoring happening, now all methods and fields for monitoring visited or unvisited cells are in other class, this class will be moved to upper level next commit

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/chess/HorseSolutionFinder.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/chess/HorseSolutionFinder.java
@@ -8,14 +8,54 @@ import java.util.function.BiPredicate;
  * Данный класс позволяет найти решения, для обхода конём шахматной доски размера NxN, таким образом, чтобы конь побывал в каждой клетке ровно один раз
  * Считается, что минимальная координата коня(из допустимых - 0, 0)
  * Отрицательных координат в данной системе не бывает
- * Начало координат расположено в каком-то углу, данному классу не важно в каком оно
+ * Начало координат расположено в левом нижнем углу
  * Координатные оси направлены из начала координат по сторонам доски
  */
 public class HorseSolutionFinder {
+    private static class CellsMonitor{
+        private final boolean[][] visitedCells;
+        private int countVisitCells = 0;
+        public CellsMonitor(int size){
+            this.visitedCells = new boolean[size][size];
+        }
+        /**
+         * Помечает заданную клетку как посещённую
+         * @param x абсцисса
+         * @param y ордината
+         */
+        private void markAsVisitedCell(int x, int y) {
+            visitedCells[x][y] = true;//отмечаем клетку как посещённую
+            countVisitCells++;//увеличиваем количество посещённых клеток
+        }
+        /**
+         * Помечает заданную клетку как не посещённую
+         * @param x абсцисса
+         * @param y ордината
+         */
+        private void markAsUnvisitedCell(int x, int y) {
+            visitedCells[x][y] = false;//отмечаем клетку как не посещённую
+            countVisitCells--;//уменьшаем количество посещённых клеток
+        }
+        public boolean isCellVisited(int x, int y){
+            return visitedCells[x][y];
+        }
+        public int getCountVisitCells() {
+            return countVisitCells;
+        }
+
+        public void reset() {
+            //сбрасываем все клетки в не посещённое состояние
+            for (var line : visitedCells) {
+                Arrays.fill(line, false);
+            }
+            countVisitCells = 0;//сбрасываем количество посещённых клеток в исходное
+        }
+    }
+
+
     private final int N;
-    private final boolean[][] visitedCells;
-    private int countVisitCells = 0;
     private final ArrayList<Coordinates> foundSolution = new ArrayList<>();
+    private final CellsMonitor cellsMonitor;
 
     /**
      * Создаёт экземпляр данного решателя
@@ -23,7 +63,7 @@ public class HorseSolutionFinder {
      */
     public HorseSolutionFinder(final int N) {
         this.N = N;
-        visitedCells = new boolean[N][N];
+        cellsMonitor = new CellsMonitor(N);
     }
 
     /**
@@ -34,7 +74,7 @@ public class HorseSolutionFinder {
      */
     public List<Coordinates> findSolution(int x, int y) {
         reset();
-        markAsVisitedCell(x, y);
+        cellsMonitor.markAsVisitedCell(x, y);
         boolean found = findSolution(getAvailableHorseMoves(x, y));
         if (found) {
             Collections.reverse(foundSolution);
@@ -50,7 +90,7 @@ public class HorseSolutionFinder {
     private boolean findSolution(Collection<Coordinates> availableMoves) {
         for (var availableMove : availableMoves) {
             int currentX = availableMove.getX(), currentY = availableMove.getY();
-            markAsVisitedCell(currentX, currentY);//помечаем данную клетку как посещённую
+            cellsMonitor.markAsVisitedCell(currentX, currentY);//помечаем данную клетку как посещённую
             Collection<Coordinates> availableHorseMoves = getAvailableHorseMoves(currentX, currentY);//получаем доступные ходы из текущей клетки
             boolean found = findSolution(availableHorseMoves);//рекурсивный вызов этой же функции
             //если мы нашли решение, добавляем текущую клетку в поле, содержащее последовательность ходов и возвращаем true
@@ -58,31 +98,13 @@ public class HorseSolutionFinder {
                 foundSolution.add(availableMove);
                 return true;
             } else {//решение не нашли
-                markAsUnvisitedCell(currentX, currentY);//помечаем данную клетку как не посещённую
+                cellsMonitor.markAsUnvisitedCell(currentX, currentY);//помечаем данную клетку как не посещённую
                 if (availableHorseMoves.isEmpty()) return false;//если нет доступных ходов из текущей клетки возвращаем false
             }
         }
-        return countVisitCells == N * N;//если мы посетили N*N клеток, возвращаем true
+        return cellsMonitor.getCountVisitCells() == N * N;//если мы посетили N*N клеток, возвращаем true
     }
 
-    /**
-     * Помечает заданную клетку как посещённую
-     * @param x абсцисса
-     * @param y ордината
-     */
-    private void markAsVisitedCell(int x, int y) {
-        visitedCells[x][y] = true;//отмечаем клетку как посещённую
-        countVisitCells++;//увеличиваем количество посещённых клеток
-    }
-    /**
-     * Помечает заданную клетку как не посещённую
-     * @param x абсцисса
-     * @param y ордината
-     */
-    private void markAsUnvisitedCell(int x, int y) {
-        visitedCells[x][y] = false;//отмечаем клетку как не посещённую
-        countVisitCells--;//уменьшаем количество посещённых клеток
-    }
     /**
      * Ищет доступные ходы из данной клетки для коня, с учётом посещённых клеток
      * @param X координата для коня
@@ -91,7 +113,7 @@ public class HorseSolutionFinder {
      */
     private Collection<Coordinates> getAvailableHorseMoves(final int X, final int Y) {
         Collection<Coordinates> result = new ArrayList<>();
-        BiPredicate<Integer, Integer> coordinatesValidator =   (x, y) -> x >= 0 && x < N && y >= 0 && y < N && !visitedCells[x][y];// эта лямбда написана таким образом, что учитывает не только границы доски, но и посещённые клетки
+        BiPredicate<Integer, Integer> coordinatesValidator =   (x, y) -> x >= 0 && x < N && y >= 0 && y < N && !cellsMonitor.isCellVisited(x,y);// эта лямбда написана таким образом, что учитывает не только границы доски, но и посещённые клетки
         //вспомогательная функция для добавления значений в результат с проверкой на валидность
         //этот трюк позволяет не создавать заведомо неверный объект координат, тем самым экономя память
         BiConsumer<Integer, Integer> addIfValid = (x, y) -> {
@@ -117,11 +139,7 @@ public class HorseSolutionFinder {
      * Сбрасывает объект поиска решений в исходное состояние
      */
     private void reset() {
-        countVisitCells = 0;//сбрасываем количество посещённых клеток в исходное
         foundSolution.clear();//очищаем список найденных решений
-        //сбрасываем все клетки в не посещённое состояние
-        for (var line : visitedCells) {
-            Arrays.fill(line, false);
-        }
+        cellsMonitor.reset();
     }
 }
