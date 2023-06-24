e6ac914298f84b785287748143cb781bb1e6fa57
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-24 19:03:02 +0400

    Removed equals overriding from Coordinates, it is unnecessary, because Coordinates is record

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/chess/Coordinates.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/chess/Coordinates.java
@@ -6,13 +6,4 @@ public record Coordinates(int x, int y) {
     public String toString() {
         return "{x=" + x + ", y=" + y + '}';
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Coordinates that = (Coordinates) o;
-        return x == that.x && y == that.y;
-    }
-
 }
