1975dee4b9f4381d29adbc89a35dd9bf26fb07f8
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-24 20:13:49 +0400

    Added vendor and model fields in Laptop class

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar6/homework/Laptop.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar6/homework/Laptop.java
@@ -2,12 +2,16 @@ package org.blbulyandavbulyan.jseminars.seminar6.homework;
 
 public class Laptop {
 //    private Long id;
+    private final String vendor;
+    private final String model;
     private final long RAMSize;
     private final long HDDSize;
     private final String OSName;
     private final String color;
 
-    public Laptop(long RAMSize, long HDDSize, String OSName, String color) {
+    public Laptop(String vendor, String model, long RAMSize, long HDDSize, String OSName, String color) {
+        this.vendor = vendor;
+        this.model = model;
         this.RAMSize = RAMSize;
         this.HDDSize = HDDSize;
         this.OSName = OSName;
@@ -28,6 +32,14 @@ public class Laptop {
 
     public String getColor() {
         return color;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public String getModel() {
+        return model;
     }
 
 }
