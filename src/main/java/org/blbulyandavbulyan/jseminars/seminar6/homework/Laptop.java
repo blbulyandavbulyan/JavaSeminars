22849bd8424c2a1de72aec11295dba7d4df1d422
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-24 20:29:58 +0400

    Added method equals and hashcode in Laptop

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar6/homework/Laptop.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar6/homework/Laptop.java
@@ -1,5 +1,7 @@
 package org.blbulyandavbulyan.jseminars.seminar6.homework;
 
+import java.util.Objects;
+
 public class Laptop {
 //    private Long id;
     private final String vendor;
@@ -46,5 +48,22 @@ public class Laptop {
     public String toString() {
         return "%s %s, RAM size: %d bytes, HDD size: %d bytes, OS family: %s, color: %s"
                 .formatted(vendor, model, getRAMSize(), getHDDSize(), getOSName(), getColor());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;//проверка на самого себя
+        if (o == null || getClass() != o.getClass()) return false;//проверка на null и проверка на несовпадение типов
+        Laptop laptop = (Laptop) o;//у нас определённо o точно не null и типы совпадают, значит можем кастить
+        //ну а дальше просто сравниваем все поля
+        return RAMSize == laptop.RAMSize && HDDSize == laptop.HDDSize &&
+                Objects.equals(vendor, laptop.vendor) && Objects.equals(model, laptop.model) &&
+                Objects.equals(OSName, laptop.OSName) && Objects.equals(color, laptop.color);
+    }
+
+    @Override
+    public int hashCode() {
+        //используем стандартный метод для комбинирования хэша, нужно так же иметь в виду, что если equals возвращает true для двух объектов, то и хэшкод должен быть одинаковый
+        return Objects.hash(vendor, model, RAMSize, HDDSize, OSName, color);
     }
 }
