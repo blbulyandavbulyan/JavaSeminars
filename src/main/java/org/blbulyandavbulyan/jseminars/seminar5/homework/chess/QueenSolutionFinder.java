b9705e779dfe44441d06ef8d06c6bda569ac1ce5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-24 19:06:32 +0400

    Removed unnecessary field from QueenSolutionFinder

--- src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/chess/QueenSolutionFinder.java
+++ src/main/java/org/blbulyandavbulyan/jseminars/seminar5/homework/chess/QueenSolutionFinder.java
@@ -4,7 +4,6 @@ import java.util.*;
 import java.util.function.Consumer;
 
 public class QueenSolutionFinder {
-    private final int N;
     private Collection<Coordinates> currentSolutionStorage;
     private final Set<Collection<Coordinates>> foundSolutions;
     private final CellsMonitor cellsMonitor;
@@ -13,8 +12,6 @@ public class QueenSolutionFinder {
      * @param N размер доски
      */
     public QueenSolutionFinder(int N){
-        this.N = N;
-
         foundSolutions = new HashSet<>();
         currentSolutionStorage = new ArrayList<>();
         cellsMonitor = new CellsMonitor(N);
